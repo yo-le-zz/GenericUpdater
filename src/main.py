18c3105651d702114126c94bf88bb129cb3cdf86
yo_le_zz <yolezz.secret@gmail.com>
2025-12-16 21:12:57 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 # main.py
+import os
+import sys
+import subprocess
 from downloader import download_latest
 from cleaner import wait_and_remove
 from rename import replace_and_restart
@@ -17,6 +20,22 @@ def main():
     wait_and_remove(exe_target)
     replace_and_restart(new_exe, exe_target)
 
+
+def self_destruct():
+    exe_path = os.path.abspath(sys.argv[0])
+
+    cmd = f'''
+    ping 127.0.0.1 -n 3 > nul
+    del "{exe_path}"
+    '''
+
+    subprocess.Popen(
+        ["cmd", "/c", cmd],
+        creationflags=subprocess.CREATE_NO_WINDOW
+    )
+
 if __name__ == "__main__":
     main()
+    self_destruct()
+    sys.exit(0)
 
